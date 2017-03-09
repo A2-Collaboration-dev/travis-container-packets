b01a70ea7f21252dc229df37ba17ecf36edd8c0a
Andreas Neiser <andreas.neiser@gmail.com>
2017-03-09 20:57:28 +0100

    Add convenience script

---
+++ setup.sh
@@ -0,0 +1,5 @@
+# source this file to setup the package
+wget https://github.com/A2-Collaboration-dev/travis-container-packets/releases/download/APLCON/APLCON.tar.bz2
+tar -xjf APLCON.tar.bz2
+rm APLCON.tar.bz2
+export APLCONSYS=$(pwd)/APLCON
