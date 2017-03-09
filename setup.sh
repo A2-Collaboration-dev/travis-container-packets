35c5b28123b66ac2bce29caa33d61bba4f33216d
Andreas Neiser <andreas.neiser@gmail.com>
2017-03-09 21:47:44 +0100

    Adapt setup.sh to new tag name

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # source this file to setup the package
-wget https://github.com/A2-Collaboration-dev/travis-container-packets/releases/download/APLCON/APLCON.tar.bz2
+wget https://github.com/A2-Collaboration-dev/travis-container-packets/releases/download/APLCON-master/APLCON.tar.bz2
 tar -xjf APLCON.tar.bz2
 rm APLCON.tar.bz2
 export APLCONSYS=$(pwd)/APLCON
