adbe78493f0850596b02ac34d6cc4bd3e8186c38
Andreas Neiser <andreas.neiser@gmail.com>
2017-03-09 21:19:20 +0100

    First version of build script

---
+++ build_gsi-pluto.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+BASEDIR=$PWD
+# Get sources
+echo "Getting sources..."
+git clone https://github.com/A2-Collaboration/gsi-pluto.git
+
+# Build library
+echo "Building..."
+cd gsi-pluto 
+make -j2
+# Tar library
+echo "Build done, tarring..."
+cd $BASEDIR
+tar -jc --file=gsi-pluto.tar.bz2 gsi-pluto
+echo "Created tarball:"
+ls -lah gsi-pluto.tar.bz2
