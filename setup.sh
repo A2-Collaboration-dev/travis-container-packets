4f4cc3eacabdc0cc8036ba67d0fe1208df8e2a04
Andreas Neiser <andreas.neiser@gmail.com>
2017-03-09 22:54:06 +0100

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # source this file to setup the package
-wget https://github.com/A2-Collaboration-dev/travis-container-packets/releases/download/gsi-pluto-master/APLCON.tar.bz2
+wget https://github.com/A2-Collaboration-dev/travis-container-packets/releases/download/gsi-pluto-master/gsi-pluto.tar.bz2
 tar -xjf gsi-pluto.tar.bz2
 rm gsi-pluto.tar.bz2
 export PLUTOSYS=$(pwd)/gsi-pluto
