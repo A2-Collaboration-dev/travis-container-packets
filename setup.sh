983c01e1573f148bb2632bf09582047811a08460
Andreas Neiser <andreas.neiser@gmail.com>
2017-03-09 22:24:05 +0100

    Add convenience script

---
+++ setup.sh
@@ -0,0 +1,5 @@
+# source this file to setup the package
+wget https://github.com/A2-Collaboration-dev/travis-container-packets/releases/download/gsi-pluto-master/APLCON.tar.bz2
+tar -xjf gsi-pluto.tar.bz2
+rm gsi-pluto.tar.bz2
+export PLUTOSYS=$(pwd)/gsi-pluto
