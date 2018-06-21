af315a324a2b41ddeec5d63c6416be0cd3778cc7
Sascha Wagner <wagners@kph.uni-mainz.de>
2018-06-21 16:37:58 +0200

    Change links to moved repository

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # source this file to setup the package
-wget https://github.com/A2-Collaboration-dev/travis-container-packets/releases/download/cern-root-v5-34-00-patches-A2/cern-root.tar.bz2
+wget https://github.com/A2-Collaboration/travis-container-packets/releases/download/cern-root-v5-34-00-patches-A2/cern-root.tar.bz2
 tar -xjf cern-root.tar.bz2
 rm cern-root.tar.bz2
 cd cern-root && source ./bin/thisroot.sh && cd ..
