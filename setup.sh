85c9830b28af3710ed51cd6670cbaa910e453294
Sascha Wagner <wagners@kph.uni-mainz.de>
2018-06-21 16:40:04 +0200

    Change links to moved repository

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # source this file to setup the package
-wget https://github.com/A2-Collaboration-dev/travis-container-packets/releases/download/APLCONpp-master/APLCONpp.tar.bz2
+wget https://github.com/A2-Collaboration/travis-container-packets/releases/download/APLCONpp-master/APLCONpp.tar.bz2
 tar -xjf APLCONpp.tar.bz2
 rm APLCONpp.tar.bz2
 export APLCONSYS=$(pwd)/APLCONpp
