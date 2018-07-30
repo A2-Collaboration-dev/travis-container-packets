2bb03eae537002ef2a5e69b4f40d6e04d6b504b9
Sascha Wagner <wagners@kph.uni-mainz.de>
2018-07-30 15:15:06 +0200

    Let wget retry to download the package up to 3 times in case of connectivity issues

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # source this file to setup the package
-wget https://github.com/A2-Collaboration/travis-container-packets/releases/download/APLCON-master/APLCON.tar.bz2
+wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 --tries=3 https://github.com/A2-Collaboration/travis-container-packets/releases/download/APLCON-master/APLCON.tar.bz2
 tar -xjf APLCON.tar.bz2
 rm APLCON.tar.bz2
 export APLCONSYS=$(pwd)/APLCON
