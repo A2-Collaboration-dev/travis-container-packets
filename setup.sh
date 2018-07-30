0e4ba2090b0e44152671ff9909f7be674fb3afa1
Sascha Wagner <wagners@kph.uni-mainz.de>
2018-07-30 15:18:41 +0200

    Let wget retry to download the package up to 3 times in case of connectivity issues

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # source this file to setup the package
-wget https://github.com/A2-Collaboration/travis-container-packets/releases/download/gsi-pluto-master/gsi-pluto.tar.bz2
+wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 --tries=3 https://github.com/A2-Collaboration/travis-container-packets/releases/download/gsi-pluto-master/gsi-pluto.tar.bz2
 tar -xjf gsi-pluto.tar.bz2
 rm gsi-pluto.tar.bz2
 export PLUTOSYS=$(pwd)/gsi-pluto
