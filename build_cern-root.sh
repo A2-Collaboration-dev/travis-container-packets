37d8146b007db7668eefb51c888504de0a17b60b
Sascha Wagner <wagners@kph.uni-mainz.de>
2018-12-05 14:37:40 +0100

    Build a ROOT6 release

--- build_cern-root.sh
+++ build_cern-root.sh
@@ -5,14 +5,17 @@ BASEDIR=$PWD
 echo "Number of processors: $(nproc)"
 # Get sources
 echo "Getting sources..."
-git clone -b v5-34-00-patches-A2 --single-branch https://github.com/A2-Collaboration/cern-root cern-root-git
+git clone -b v6-14-00-patches --single-branch http://github.com/root-project/root.git cern-root-git
+cd cern-root-git
+git checkout -b v6-14-06 v6-14-06
+cd ..
 
 # Build library
 echo "Configuring..."
 mkdir -p $BASEDIR/cern-root-build
 mkdir -p $BASEDIR/cern-root
 cd cern-root-build
-cmake ../cern-root-git -DCMAKE_INSTALL_PREFIX=$BASEDIR/cern-root
+cmake ../cern-root-git -DCxx14=ON -Dbuiltin_lzma=OFF -Dbuiltin_zlib=OFF -Dfortan=ON -Dmathmore=ON -DCMAKE_INSTALL_PREFIX=$BASEDIR/cern-root
 echo "Building..."
 # make really outputs so much that travis aborts,
 # so convert it to dots (STDERR is still seen)
@@ -21,6 +24,6 @@ make -j2 install
 # Tar library
 echo "Build done, tarring..."
 cd $BASEDIR
-tar -jc --file=cern-root.tar.bz2 cern-root
+tar Jc --file=cern-root6.tar.xz cern-root
 echo "Created tarball:"
-ls -lah cern-root.tar.bz2
+ls -lah cern-root6.tar.xz
--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # source this file to setup the package
-wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 --tries=3 https://github.com/A2-Collaboration/travis-container-packets/releases/download/cern-root-v5-34-00-patches-A2/cern-root.tar.bz2
-tar -xjf cern-root.tar.bz2
-rm cern-root.tar.bz2
+wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 --tries=3 https://github.com/A2-Collaboration/travis-container-packets/releases/download/cern-root-v6-14-06/cern-root6.tar.xz
+tar xJf cern-root6.tar.xz
+rm cern-root6.tar.xz
 cd cern-root && source ./bin/thisroot.sh && cd ..
